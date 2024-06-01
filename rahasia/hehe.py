58823294aab48f4e4d261ec7f0b3d98b49f5fc3a
ipkt <putu.kembar@gmail.com>
2024-06-01 12:06:06 +0800

    update git ignire

---
+++ rahasia/hehe.py
@@ -0,0 +1,15 @@
+def koneksidb ():
+    host = "localhost"
+    user = "root"
+    password = ""
+    database = "aloptamadnp"
+    return host,user,password,database
+
+def myToken ():
+    token = "7193995386:AAEHW6pBcZgI4EBWy2jB8MGsOYEvfsTvVqE"
+    return token
+
+def userpass():
+    user = "geofisika"
+    sandi = "sanglah12345"
+    return  user , sandi
